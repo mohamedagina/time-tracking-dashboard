4fdc2ef2558b91ddaba2614cf803cc5f209706ed
mohamedagina <mohamedagina001@gmail.com>
2023-02-04 22:42:26 +0200

    style: Change layout

--- js/main.js
+++ js/main.js
@@ -2,34 +2,40 @@ import tasks from '../util/data.json' assert { type: 'json' };
 
 const timeFrame = location.search.substring(1) || 'weekly';
 
-const tasksContainer = document.createElement('div');
-tasksContainer.classList.add('tasks-container');
+document
+  .querySelector(`.app-nav > a[data-time-frame=${timeFrame}]`)
+  .classList.add('active');
 
 const taskCards = tasks
   .map(
     task =>
       `
     <div
-      class="task-card ${task.title.replaceAll(' ', '-')}"
+      class="task-card ${task.title.toLowerCase().replaceAll(' ', '-')}"
       style ="background-image: url('${task.icon}')">
 
-      <header class="card-header">
-        <span class="task-title">${task.title}</span>
-        <button class="options-button"><img src="../images/icon-ellipsis.svg"/></button>
-      </header>
+      <div>
+        <header class="card-header">
+          <span>${task.title}</span>
 
-      <span class="current-hours">${task.timeframes[timeFrame].current}hrs
-      </span>
+          <button class="options-button">
+            <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"  fill-rule="evenodd"/></svg>
+          </button>
+        </header>
+        
+        <div class="no-of-hours">
+          <span class="current-hours">${task.timeframes[timeFrame].current}hrs
+          </span>
 
-      <span class="previous-hours">
-      last
-        ${timeFrame === 'daily' ? 'yesterday' : timeFrame.replace('ly', '')}
-        - ${task.timeframes[timeFrame].previous}hrs
+          <span class="previous-hours">
+          last
+            ${timeFrame === 'daily' ? 'yesterday' : timeFrame.replace('ly', '')}
+            - ${task.timeframes[timeFrame].previous}hrs
+        </div>
+      </div> 
     </div>
     `
   )
   .join('');
 
-tasksContainer.innerHTML = taskCards;
-
-document.body.appendChild(tasksContainer);
+document.querySelector('.dashboard').innerHTML += taskCards;
